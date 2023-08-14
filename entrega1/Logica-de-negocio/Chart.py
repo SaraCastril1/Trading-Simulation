282f0fc0601f298cd82fa0c90ad8a9c7c21e3880
SaraCastril1 <smcastril1@eafit.edu.co>
2023-08-14 08:42:39 -0500

    Graficaciòn en tiempo real. NO Borra velas procesadas

--- entrega1/Logica-de-negocio/Chart.py
+++ entrega1/Logica-de-negocio/Chart.py
@@ -1,16 +1,32 @@
-import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from mplfinance.original_flavor import candlestick_ohlc
+import time
+import random
+from datetime import timedelta
 
 class Chart:
-    
+
     def __init__(self):
-        self.fig = go.Figure()  # Crear una instancia del gráfico al inicio
+        self.data = []
 
-    def crear_grafico(self, timestamp, open_price, high_price, low_price, close_price):
-        # Añadir una nueva vela al gráfico existente
-        self.fig.add_trace(go.Candlestick(x=[timestamp],
-                                          open=[open_price],
-                                          high=[high_price],
-                                          low=[low_price],
-                                          close=[close_price]))
-
-        self.fig.show()
+    def graficar_velas_chinas(self):
+        plt.ion()
+        fig, ax = plt.subplots()
+        index = 0  # Índice para rastrear la posición actual en los datos
+        while index < len(self.data):  # Condición de salida cuando no hay más datos
+            ax.clear()
+            # Tomar un segmento de datos hasta el índice actual
+            segmento_data = self.data[:index+1]
+            fechas_num = [mdates.date2num(item['Fecha']) for item in segmento_data]
+            quotes = [(fechas_num[i], float(item['Apertura']), float(item['Alto']), float(item['Bajo']), float(item['Cierre'])) for i, item in enumerate(segmento_data)]
+            
+            ax.xaxis_date()
+            ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m-%d"))
+            plt.xticks(rotation=45)
+            
+            candlestick_ohlc(ax, quotes, width=0.02, colorup='g', colordown='r')
+            
+            plt.pause(1)  # Pausa para una actualización más lenta (puedes ajustar este valor)
+            
+            index += 1  # Incrementar el índice para avanzar a través de los datos
--- entrega1/Logica-de-negocio/Scanner.py
+++ entrega1/Logica-de-negocio/Scanner.py
@@ -1,49 +1,56 @@
 import csv
 import gc
+import json
 from Chart import Chart
+import matplotlib.pyplot as plt
+from datetime import datetime, timedelta
+import time
 
 class Scanner:
 
     def __init__(self):
         self.chart = Chart()  # Crear una instancia única de la clase Chart
 
-
-
     def leer_csv(self, ruta):
-        #datos_todos = []
-
         try:
             with open(ruta, 'r') as f:
-                lector = csv.reader(f)
-                for fila in lector:
-                    # timestamp = fila[0]
-                    # open_price = float(fila[1])
-                    # high_price = float(fila[2])
-                    # low_price = float(fila[3])
-                    # close_price = float(fila[4])
-                    #crear_grafico(fila[0], float(fila[1]), float(fila[2]), float(fila[3]), float(fila[4]))
-
-                    #datos_todos.append(fila)
-                    #print(fila)
-                    self.chart.crear_grafico(fila[0], float(fila[1]), float(fila[2]), float(fila[3]), float(fila[4]))
+                reader = csv.reader(f)
+                next(reader)
+                for row in reader:
+                    self.chart.data.append({
+                        'Fecha': datetime.strptime(row[0], "%Y-%m-%d %H:%M"),
+                        'Apertura': row[1],
+                        'Alto': row[2],
+                        'Bajo': row[3],
+                        'Cierre': row[4]
+                    })
+            self.chart.graficar_velas_chinas()
+            f.close()
 
         except FileNotFoundError:
-            print("El archivo no se encontró en la ruta especificada.")
+            print("No se encontró en la ruta especificada.")
         except Exception as e:
             print("Ocurrió un error:", str(e))
 
-        
-
-        
-        del lector    # Liberar manualmente el objeto lector
-        gc.collect()  # Invocar al recolector de basura
-        print("Se ha eliminado el lector csv de la memoria")
-
-        del chart    # Liberar manualmente el objeto lector
-        gc.collect()  # Invocar al recolector de basura
-        print("Se ha eliminado el objeto chart de la memoria")
-
-
-
     def leer_json(self, ruta):
-        print("Soy JSON")
+        print("Leyendo archivo JSON...")
+        try:
+            with open(ruta, 'r') as f:
+                contenido = json.load(f)
+                fecha_inicial = datetime(2000, 1, 1)
+                for i in range(len(contenido['time'])):
+                    fecha = fecha_inicial + timedelta(minutes=contenido['time'][i])
+                    self.chart.data.append({
+                        'Fecha': fecha,
+                        'Apertura': contenido['open'][i],
+                        'Alto': contenido['high'][i],
+                        'Bajo': contenido['low'][i],
+                        'Cierre': contenido['close'][i]
+                    })
+            self.chart.graficar_velas_chinas()
+            f.close()
+
+        except FileNotFoundError:
+            print("No se encontró en la ruta especificada.")
+        except Exception as e:
+            print("Ocurrió un error:", str(e))
