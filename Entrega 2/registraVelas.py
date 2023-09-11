1f5d236db93b585b6d39f18e1851c5539a79e6c0
SaraCastril1 <smcastril1@eafit.edu.co>
2023-09-10 22:55:59 -0500

    Celery

---
+++ Entrega 2/registraVelas.py
@@ -0,0 +1,23 @@
+import os
+from celery import Celery, shared_task
+from dotenv import load_dotenv
+import datetime
+import sqlite3
+import json
+
+
+load_dotenv()
+
+#Declara el app de celery
+app = Celery('registra-velas',
+             broker = 'amqp://{}:{}@54.85.196.208:5672/sara_vhost'.format(os.environ.get('user'), os.environ.get('password')))
+
+#Declara un fichero sqlite
+now = datetime.datetime.now()
+registro = sqlite3.connect("comandos-{}-{}-{}-{}-{}-{}.db".format(now.year, now.month, now.day, now.hour, now.minute ))
+registro.execute('''CREATE TABLE IF NOT EXISTS registro
+                 (repeticiones INT, comando VARCHAR(32) unique)''') 
+
+#Declara la tarea
+@shared_task #Función que devuelve otra función
+def registro(comando):
