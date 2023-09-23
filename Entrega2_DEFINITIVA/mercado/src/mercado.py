eb4dec5fda99a48d4c62d55564195c0af0d4672f
SaraCastril1 <smcastril1@eafit.edu.co>
2023-09-23 06:28:40 -0500

    Puertos mercados completos

--- Entrega2_DEFINITIVA/mercado/src/mercado.py
+++ Entrega2_DEFINITIVA/mercado/src/mercado.py
@@ -39,7 +39,7 @@ def server(PORT):
 def main():
     load_dotenv()
 
-    if len(sys.argv) >= 2:
+    if len(sys.argv) >= 3:
 
         if sys.argv[2] == '1':
             PORT = os.environ.get("PORT-1") 
