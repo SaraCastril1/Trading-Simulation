d99191b633740f44986fafc0ff0578f3cd3af751
SaraCastril1 <smcastril1@eafit.edu.co>
2023-09-23 06:14:54 -0500

    Puertos mercados completos

--- Entrega2_DEFINITIVA/mercado/src/mercado.py
+++ Entrega2_DEFINITIVA/mercado/src/mercado.py
@@ -41,23 +41,23 @@ def main():
 
     if len(sys.argv) > 1:
 
-        if sys.argv[1] == 1:
+        if sys.argv[2] == 1:
             PORT = os.environ.get("PORT-1") 
-        elif sys.argv[1] == 2:
+        elif sys.argv[2] == 2:
             PORT = os.environ.get("PORT-2") 
-        elif sys.argv[1] == 3:
+        elif sys.argv[2] == 3:
             PORT = os.environ.get("PORT-3") 
-        elif sys.argv[1] == 4:
+        elif sys.argv[2] == 4:
             PORT = os.environ.get("PORT-4") 
-        elif sys.argv[1] == 5:
+        elif sys.argv[2] == 5:
             PORT = os.environ.get("PORT-5") 
-        elif sys.argv[1] == 6:
+        elif sys.argv[2] == 6:
             PORT = os.environ.get("PORT-6") 
-        elif sys.argv[1] == 7:
+        elif sys.argv[2] == 7:
             PORT = os.environ.get("PORT-7") 
-        elif sys.argv[1] == 8:
+        elif sys.argv[2] == 8:
             PORT = os.environ.get("PORT-8") 
-        elif sys.argv[1] == 9:
+        elif sys.argv[2] == 9:
             PORT = os.environ.get("PORT-9") 
         
         else:
